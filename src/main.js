020caa7b35e35e10e4be88ff71bdbaf06c23c04d
ammarr <ammarrusesthis80@gmail.com>
2026-01-03 12:48:28 +0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { CheckCheck, CircleCheckBig, createIcons, TrendingUp } from "lucide";
+
+document.addEventListener("DOMContentLoaded", () => {
+  createIcons({
+    icons: {
+      TrendingUp, CheckCheck, CircleCheckBig, Clock3
+    },
+  });
+});
